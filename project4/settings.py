011cd36ac45e60c91af4be6b9294b3eeab9ab631
giankpetrov <>
2023-05-11 17:24:14 +0000

    ADD reservations app to settings

--- project4/settings.py
+++ project4/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'cloudinary_storage',
     'django.contrib.staticfiles',
-    'cloudinary'
+    'cloudinary',
+    'reservations',
 ]
 
 MIDDLEWARE = [
