112f6ddddc50be1335e6407ce95ae7aa0e2ab110
giankpetrov <>
2023-05-15 23:26:56 +0000

    ADD filters and list to Django Admin Area

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,8 +1,30 @@
 from django.contrib import admin
 from .models import Users, Reservations, Restaurants, Tables, Timeslots
 
-admin.site.register(Users)
-admin.site.register(Reservations)
-admin.site.register(Restaurants)
-admin.site.register(Tables)
+
 admin.site.register(Timeslots)
+
+@admin.register(Users)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ('UserID', 'Name', 'Email', 'UserType')
+    ordering = ('Name',)
+    search_fields = ('Name', 'Email')
+
+@admin.register(Reservations)
+class ReservationsAdmin(admin.ModelAdmin):
+    list_display = ('ReservationID', 'UserID', 'RestaurantID', 'TimeslotID', 'ReservationDate')
+    ordering = ('ReservationID',)
+    search_fields = ('ReservationID', 'RestaurantID__Name')
+    list_filter = ('RestaurantID', 'TimeslotID', 'ReservationDate')
+
+@admin.register(Restaurants)
+class RestaurantsAdmin(admin.ModelAdmin):
+    list_display = ('RestaurantID', 'Name', 'Address')
+    ordering = ('Name',)
+    list_filter = ('Name', 'Address')
+
+@admin.register(Tables)
+class TablesAdmin(admin.ModelAdmin):
+    list_display = ('RestaurantID', 'TableID', 'MaxCapacity')
+    ordering = ('MaxCapacity',)
+    list_filter = ('RestaurantID__Name', 'MaxCapacity')
