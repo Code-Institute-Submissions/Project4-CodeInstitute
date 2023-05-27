42c68f248988537efa391e3074d596428eb005a4
giankpetrov <>
2023-05-27 00:23:40 +0000

    ADD register user functionality

--- project4/urls.py
+++ project4/urls.py
@@ -25,7 +25,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('reservations/', reservations.views.reservations,
          name='reservations'),
-    path('signup/', reservations.views.signup, name='signup'),
     path('update_reservation/<reservation_id>',
          reservations.views.update_reservation,
          name='update-reservation'),
